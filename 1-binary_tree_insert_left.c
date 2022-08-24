4052a11eb2b8fc7926419c7b78791609fe86820c
belovetech <belovetech@gmail.com>
2022-08-24 21:05:12 +0100

    Check the second function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * binary_tree_insert_left2 - Inserts a node as the left-child of another node
  *
  * @parent: Pointer to the node to insert the left-child in
  * @value: Value to put in the new node
  * Return: Pointer to the new node otherwise NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left2(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
@@ -27,13 +27,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 }
 
 /**
- * binary_tree_insert_left2 - Inserts a node as the left-child of another node
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
  *
  * @parent: Pointer to the node to insert the left-child in
  * @value: Value to put in the new node
  * Return: Pointer to the new node otherwise NULL
  */
-binary_tree_t *binary_tree_insert_left2(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
