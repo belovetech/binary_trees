52664014e988ea6825c79e5cefcb5fce3d1135b5
belovetech <belovetech@gmail.com>
2022-08-24 21:08:17 +0100

    Update the binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,32 +7,6 @@
  * Return: Pointer to the new node otherwise NULL
  */
 
-binary_tree_t *binary_tree_insert_left2(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node = NULL;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new_node = binary_tree_node(parent, value);
-	new_node->left = parent->left;
-
-	if (parent->left != NULL)
-	{
-		parent->left->parent = new_node;
-	}
-	parent->left = new_node;
-
-	return (new_node);
-}
-
-/**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
- *
- * @parent: Pointer to the node to insert the left-child in
- * @value: Value to put in the new node
- * Return: Pointer to the new node otherwise NULL
- */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
@@ -40,11 +14,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = parent->left;
+	new_node->right = NULL;
 
 	if (parent->left != NULL)
 	{
-		binary_tree_insert_left(new_node, parent->left->n);
+		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
 
