2de4033b504607df23fe9979ad29443c8cfc3bc6
belovetech <belovetech@gmail.com>
2022-08-24 13:12:00 +0100

    Insert a node as the left-child of another node

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -26,3 +31,10 @@ typedef struct binary_tree_s avl_t;
 
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
+
+/* Prototypes */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */
