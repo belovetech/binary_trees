b435b0fdd4e550d7a4dd153c00ca5db34fd9e559
belovetech <belovetech@gmail.com>
2022-08-25 21:49:40 +0100

    Fix betty error

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,16 +3,16 @@
 /**
  * binary_tree_sibling - Finds the sibling of a node
  *
- * @tree: Pointer to the node to find the sibling
+ * @node: Pointer to the node to find the sibling
  *
  * Return: If node is NULL return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-        return (NULL);
-        
-    if (node->parent->left == node)
-        return (node->parent->right);
-    return(node->parent->left);
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
